215799479a7cd25729140ccc8f3438b043484234
“preetijojo” <joanbrahim@hotmail.com>
2023-04-21 08:31:52 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - puts program
+ * return: 0 (success)
+ * */
+Int main (void) 
+{
+	puts (“\” Programming is like building a multilingual puzzle”) ;
+	return (0) ;
+}
