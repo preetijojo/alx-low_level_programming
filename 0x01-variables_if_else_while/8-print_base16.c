c1a34eab3fe771ea69bcf7359df262e781472f98
“preetijojo” <joanbrahim@hotmail.com>
2023-04-21 23:26:30 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry for the program
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char ch;
+
+	for (i = 0; i < 16; i++)
+	{
+		if (i < 10)
+		{
+		ch = 1 + '0';
+	{
+		else
+			ch = 1 - 10 + 'a';
+	}
+	putchar(ch);
+}
+putchar('\n');
+return (0);
+}
