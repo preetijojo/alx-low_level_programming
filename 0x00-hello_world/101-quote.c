d40d3ea07b6688e1ea077a370536996b5fbb7eb3
“preetijojo” <joanbrahim@hotmail.com>
2023-04-22 01:14:22 -0700

    task 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry of the program
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, str, 59);
+	return (1);
+}
