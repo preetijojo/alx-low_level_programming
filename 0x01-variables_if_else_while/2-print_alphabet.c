eb7aafe33dc0bd81cd03a77036819f65622e8e65
“preetijojo” <joanbrahim@hotmail.com>
2023-04-21 21:46:33 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - ENtry point of the program
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int 1;
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n')
+
+		return (0);
+}
