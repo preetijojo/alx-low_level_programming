a90460ff6bf17f9ee1f3edaf3413e55c0f433695
“preetijojo” <joanbrahim@hotmail.com>
2023-04-23 10:26:06 -0700

    task 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry for program
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i, t;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (t = i; t <= 99; t++)
+		{
+			if (i != t)
+			{
+				putchar(i / 10 + '0');
+				putchar(i % 10 + '0');
+				putchar(' ');
+				putchar(t / 10 + '0');
+				putchar(j % 10 + '0');
+				if (i != 98 || t != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
