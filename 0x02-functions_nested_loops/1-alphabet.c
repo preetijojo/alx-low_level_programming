ad50888f844ef8c9b2a1750d9185088426b63171
“preetijojo” <joanbrahim@hotmail.com>
2023-04-25 12:52:25 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+		if (c == 'z' + 1)
+		{
+			_putchar('\n');
+		}
+	}
+}
