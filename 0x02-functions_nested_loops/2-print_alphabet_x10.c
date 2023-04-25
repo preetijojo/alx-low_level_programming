b86b589199d4f2f9421a6cde3a323b112bffa5c6
“preetijojo” <joanbrahim@hotmail.com>
2023-04-25 15:32:54 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,13 +5,13 @@
  */
 void print_alphabet_x10(void)
 {
-	char letter = 'a';
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 26; j++)
 		{
-			_putchar(letter + j);
+			_putchar('a' + j);
 		}
 		_putchar('\n');
 	}
