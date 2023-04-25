8fc31f3cd1bec1d7cc15f2348daee41c2d37b5c0
“preetijojo” <joanbrahim@hotmail.com>
2023-04-25 15:55:36 -0700

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _islower - checks for lowercase character
+ * @c: The character in ASCII code
  * Return: 1 for lowercase character. 0 for the rest
  */
 int _islower(int c)
