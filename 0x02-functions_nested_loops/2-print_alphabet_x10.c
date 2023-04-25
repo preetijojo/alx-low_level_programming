fb5cdd6f94eb05cf05c2129fef8fbea44d7ef8cf
“preetijojo” <joanbrahim@hotmail.com>
2023-04-25 13:13:46 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet - prints 10 times the alphabet in lowercase
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase
  * Return: Always 0
  */
 void print_alphabet_x10(void)
