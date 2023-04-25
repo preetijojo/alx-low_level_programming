7d3c3e3a1115424c193ab8aaca239ca013fc8189
“preetijojo” <joanbrahim@hotmail.com>
2023-04-25 13:47:46 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,13 +12,14 @@ void print_alphabet_x10(void)
 	{
 		while (c <= 'z')
 		{
-			_putchar(c); c++;
+			_putchar(c);
+			c++;
 		}
-		c = 'a'; i++;
+		c = 'a';
+		i++;
 		if (i == 10 || (i == 9 && c == 'z'))
 		{
 			_putchar('\n');
 		}
 	}
 }
-
