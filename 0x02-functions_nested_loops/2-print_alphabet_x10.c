2ecbdd612b93fc9dd7dfd6f235c1312b1d4bea21
“preetijojo” <joanbrahim@hotmail.com>
2023-04-25 13:11:58 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet - prints 10 times the alphabet in lowercase
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+	int i = 0;
+
+	while (i < 10)
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		c = 'a';
+		i++;
+	}
+	_putchar('\n');
+}
