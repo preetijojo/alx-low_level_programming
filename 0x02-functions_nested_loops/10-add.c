24754ab9a8321cdf3a855ca8cdcc64085baad99c
“preetijojo” <joanbrahim@hotmail.com>
2023-04-25 18:13:01 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - that adds two integers
+ * @a: the first number
+ * @b: the second number
+ * Return: addition of both
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
