7453745d1a7e9894113a207ff6be46bf43253638
“preetijojo” <joanbrahim@hotmail.com>
2023-04-26 08:52:59 -0700

    task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,12 +10,12 @@ void print_times_table(int n)
 	{
 		return;
 	}
-	for (int i = 0; i <= n; i++)
+	for (i = 0; i <= n; i++)
 	{
-		for (int j = 0; j <= n; j++)
+		for (j = 0; j <= n; j++)
 		{
 			printf("%d ", i * j);
 		}
-		pintf("\n");
+		printf("\n");
 	}
 }
