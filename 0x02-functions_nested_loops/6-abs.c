191a40643120050440db572327f5e59e0d1d807f
“preetijojo” <joanbrahim@hotmail.com>
2023-04-25 17:09:45 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - The value of an integer
+ * @n: The input number as an integer.
+ * Return: Absolute value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
