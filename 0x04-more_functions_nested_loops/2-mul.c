608aaa65ab9c6becf337a40792f3b7a51848723d
“preetijojo” <joanbrahim@hotmail.com>
2023-04-27 15:15:36 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - check the code
+ * @a: multiply with b and returns their product
+ * @b: multiply with a and return their product
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	printf("(%d)", result);
+	return (0);
+}
