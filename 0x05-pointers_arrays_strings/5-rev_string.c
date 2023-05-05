357b9cea12cfcd4b3d473e132dc1e6244c3193f0
“preetijojo” <joanbrahim@hotmail.com>
2023-05-04 22:38:50 -0700

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - function that reverses a string.
+ * @s: string
+ * return: string in reverse
+ */
+
+void rev_string(char *s)
+{
+	char rev = 
