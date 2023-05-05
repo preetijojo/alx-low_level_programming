3824ab6ca670e57a9b288f379e8e8918a75bd232
“preetijojo” <joanbrahim@hotmail.com>
2023-05-05 09:12:14 -0700

    task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_number - function that prints an integer.
+ * @n: int
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	putchar(n % 10 + '0');
+}
