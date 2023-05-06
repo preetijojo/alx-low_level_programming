0d55b310ae8822fabd54f2057c882243012c0f6b
“preetijojo” <joanbrahim@hotmail.com>
2023-05-05 20:12:48 -0700

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,12 +10,12 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		n = -n;
 	}
 	if (n / 10 != 0)
 	{
 		print_number(n / 10);
 	}
-	putchar('0' + n % 10);
+	_putchar('0' + n % 10);
 }
